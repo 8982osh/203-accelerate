ff24fbf4fd6ea762b457e63029ba187eccbfd900
8982osh <cruztoyota@gmail.com>
2020-05-05 15:17:12 -0500

    Working on adding case studies to front-page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -17,14 +17,36 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url(); ?>/blog)">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
+
+<section class="featured-work">
+	<div class="site-content">
+		<h4>Featured Work</h4>
+	
+	    <?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+	  	    <?php while ( have_posts() ) : the_post();
+	  		    $size = "medium";
+	  		    $image_1 = get_field('image_1');    
+	  	?>
+           <figure>
+           	 <?php echo wp_get_attachment_image($image_1, $size); ?>
+           </figure>
+
+
+	  	  <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+	  	<?php endwhile; ?> <!-- end loop -->
+	  	<?php wp_reset_query(); ?>
+
+	</div>
+</section>
+
 <section class="recent-posts">
 	<div class="site-content">
 	  <div class="blog-posts">
-	  	<h4>From the Blog</h4>
+	  	<h4>From the Blog!</h4>
 	  	<?php query_posts('posts_per_page=1'); ?>
 	  	<?php while ( have_posts() ) : the_post(); ?>
 	  	  <h3><?php the_title(); ?><h3>	
